8358fc99e90bd4a881c428239117f9470d70c3ee
Alan Garduño P <AlansGP@Outlook.com>
2020-06-24 15:18:28 -0500

    UPDATE

--- mp_notifications.php
+++ mp_notifications.php
@@ -1,5 +1,8 @@
 <?php
+header("HTTP/1.1 200 OK");
 require 'vendor/autoload.php';
+
+echo file_get_contents('php://input');
 
 $ch = curl_init("https://api.mercadopago.com/v1/payments/".$preference->id."?access_token=APP_USR-8058997674329963-062418-89271e2424bb1955bc05b1d7dd0977a8-592190948");
 $fp = fopen("test.txt", "w");
