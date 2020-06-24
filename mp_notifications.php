9fbc4e8db69d76bc84c27a2499e05a414854be74
Alan Garduño P <AlansGP@Outlook.com>
2020-06-24 15:02:36 -0500

    update

--- mp_notifications.php
+++ mp_notifications.php
@@ -1,6 +1,16 @@
 <?php
-$file = fopen("test.txt","a");
-fwrite($file,"-------------------------------------------- \r\n");
+
+$TextoPeticionesGoogle = "test.txt";
+//Obtiene el ultimo digito de peticiones totales
+$myfile = fopen($TextoPeticionesGoogle, "a+") or die("Unable to open file!");
+//Aumenta en uno la nueva peticion ya teniendo guardada el ultimo estado de peticiones totales
+$new = intval(fgets($myfile)) + 1;
+//Cierra el archivo
+fclose($myfile);
+//Escribe el numero total de peticiones nuevo aumentado ya en uno
+fwrite($myfile, $new);
+
+
 /*header("HTTP/1.1 200 OK");
 echo file_get_contents('php://input');
 $json = file_get_contents('php://input');
