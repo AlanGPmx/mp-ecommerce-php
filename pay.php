94c0e6762872f60697f6f87099eadf60ba521d85
Alan Garduño P <AlansGP@Outlook.com>
2020-06-24 02:20:40 -0500

    Update Data Fake

--- pay.php
+++ pay.php
@@ -3,7 +3,7 @@ error_reporting(E_ALL);
 ini_set('display_errors', '1');
 require 'vendor/autoload.php';
 
-$access_token = 'APP_USR-6718728269189792-112017-dc8b338195215145a4ec035fdde5cedf-491494389';//'APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398'; //'APP_USR-6718728269189792-112017-dc8b338195215145a4ec035fdde5cedf-491494389'; 
+$access_token = 'APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398'; //'APP_USR-6718728269189792-112017-dc8b338195215145a4ec035fdde5cedf-491494389'; 
 $integrator_id = '​dev_24c65fb163bf11ea96500242ac130004';
 $public_key = 'APP_USR-5b9a3e27-3852-407d-8f49-e08bd5990007';
 $clientID = '491494389';
@@ -11,8 +11,8 @@ $url = 'https://alangpmx-mp-ecommerce-php.herokuapp.com';
 
 MercadoPago\SDK::setAccessToken($access_token);
 MercadoPago\SDK::setIntegratorId($integrator_id);
-//MercadoPago\SDK::setPublicKey($public_key);
-//MercadoPago\SDK::setClientId($clientID);
+MercadoPago\SDK::setPublicKey($public_key);
+MercadoPago\SDK::setClientId($clientID);
 
 /* Armo preferencia de pago */
 $preference = new MercadoPago\Preference();
@@ -23,7 +23,7 @@ $preference->back_urls = array(
     "pending" => $url."/pago-pendiente.php"
 );
 $preference->auto_return = "approved";
-$preference->external_reference = 'leo.j.diaz@gmail.com';
+$preference->external_reference = 'alansgp@outlook.com';
 $preference->notification_url = $url.'/mp_notifications.php';
 $preference->payment_methods = array(
     "excluded_payment_methods" => array(
@@ -37,17 +37,18 @@ $preference->payment_methods = array(
 
 /* ARMO PERFIL DE COMPRADOR */
 $payer = new MercadoPago\Payer();
+$payer->id = 535650015;
 $payer->name = "Lalo";
 $payer->surname = "Landa";
-$payer->email = "test_user_63274575@testuser.com";
+$payer->email = "test_user_58295862@testuser.com";
 $payer->phone = array(
-    "area_code" => "11",
-    "number" => "22223333"
+    "area_code" => "52",
+    "number" => "​5549737300"
 );
 $payer->address = array(
-    "street_name" => "False",
-    "street_number" => 123,
-    "zip_code" => "1111"
+    "street_name" => "Insurgentes Sur",
+    "street_number" => 1602,
+    "zip_code" => "0394​0"
 );
 $preference->payer = $payer;
 
@@ -56,7 +57,7 @@ $preference->payer = $payer;
 $item = new MercadoPago\Item();
 $item->id = 1234;
 $item->title = $_POST['title'];
-$item->description = 'Dispositivo móvil de Tienda e-commerce MX';
+$item->description = 'Dispositivo móvil de Tienda e-commerce';
 $item->quantity = 1;
 $item->unit_price = $_POST['price'];
 $item->picture_url = $url.str_replace("./","",$_POST['img']);
