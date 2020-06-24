da9f594d0b6e2708eee23d92da3eb33ce3e8a3fe
Alan Garduño P <AlansGP@Outlook.com>
2020-06-24 14:58:45 -0500

    update

--- mp_notifications.php
+++ mp_notifications.php
@@ -1,11 +1,11 @@
 <?php
 fwrite($file,"-------------------------------------------- \r\n");
-header("HTTP/1.1 200 OK");
+/*header("HTTP/1.1 200 OK");
 echo file_get_contents('php://input');
 $json = file_get_contents('php://input');
 $file = fopen("test.txt","a");
 fwrite($file,"-------------------------------------------- \r\n");
 fwrite($file,"".$json." \r\n");
 
-var_dump($json);
+var_dump($json);*/
 ?>
