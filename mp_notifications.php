4ba39baa4919dcb21662942e08cdfee5ded2fe9a
Alan Garduño P <AlansGP@Outlook.com>
2020-06-24 15:27:49 -0500

    UPDATE

--- mp_notifications.php
+++ mp_notifications.php
@@ -1,25 +1,9 @@
 <?php
 header("HTTP/1.1 200 OK");
-require 'vendor/autoload.php';
-
-echo file_get_contents('php://input');
-
-$ch = curl_init("https://api.mercadopago.com/v1/payments/".$preference->id."?access_token=APP_USR-8058997674329963-062418-89271e2424bb1955bc05b1d7dd0977a8-592190948");
-$fp = fopen("test.txt", "w");
-
-curl_setopt($ch, CURLOPT_FILE, $fp);
-curl_setopt($ch, CURLOPT_HEADER, 0);
-
-curl_exec($ch);
-curl_close($ch);
-fclose($fp);
-
-/*header("HTTP/1.1 200 OK");
-echo file_get_contents('php://input');
 $json = file_get_contents('php://input');
 $file = fopen("test.txt","a");
 fwrite($file,"-------------------------------------------- \r\n");
 fwrite($file,"".$json." \r\n");
 
-var_dump($json);*/
+var_dump($json);
 ?>
