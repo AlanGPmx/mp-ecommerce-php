3434eafd0a370b113de47b91a737d356aa7323af
Alan Garduño P <AlansGP@Outlook.com>
2020-06-24 03:03:05 -0500

    otro commit

--- mp_notifications.php
+++ mp_notifications.php
@@ -1,5 +1,5 @@
 <?php
-//header("HTTP/1.1 200 OK");
+header("HTTP/1.1 200 OK");
 $json = file_get_contents('php://input');
 $file = fopen("test.txt","a");
 fwrite($file,"-------------------------------------------- \r\n");
