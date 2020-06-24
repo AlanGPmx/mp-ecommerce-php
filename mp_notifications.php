3eb2bbcc24706b18240270b35a542e182e1fb6f5
Alan Garduño P <AlansGP@Outlook.com>
2020-06-24 04:25:21 -0500

    .

--- mp_notifications.php
+++ mp_notifications.php
@@ -1,3 +1,4 @@
+
 <?php
 header("HTTP/1.1 200 OK");
 $json = file_get_contents('php://input');
@@ -7,3 +8,6 @@ fwrite($file,"".$json." \r\n");
 
 var_dump($json);
 ?>
+
+<script src="https://secure.mlstatic.com/sdk/javascript/v1/mercadopago.js"></script>
+
